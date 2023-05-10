a50e5bee685996b75de5a987f02f490373591e6d
Riccardo Minella <67193261+RiccardoMinella@users.noreply.github.com>
2023-05-10 20:50:41 +0200

    modifiche utente

--- backend/app/calendari.js
+++ backend/app/calendari.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const calendario = require('./models/calendario');
+const Calendario = require('./models/calendario');
 
-
+// get tutti i calendari
 router.get('', async (req, res) => {
     
     // https://mongoosejs.com/docs/api.html#model_Model.find
-    let calendari = await calendario.find({});
+    let calendari = await Calendario.find({});
 
     calendari = calendari.map( (calendario) => {
         return {
@@ -18,9 +18,47 @@ router.get('', async (req, res) => {
     res.status(200).json(calendari);
 });
 
-router.get('/:id',)
+// get calendari per id calendario
+router.get('/:id', async (req, res) => {
+
+    let calendario = await Calendario.findById(req.params.id);
+
+    res.status(200).json({
+        self: 'api/v1/calendari/' + calendario.idCalendario,
+        nome: calendario.nome,
+        appuntamenti: calendario.appuntamenti
+    })
+})
 
 
+router.delete('/:id', async (req, res) => {
+    let calendario = await Calendario.findById(req.params.id).exec();
+    if (!calendario) {
+        res.status(404).send()
+        console.log('calendar not found')
+        return;
+    }
+
+    await calendario.deleteOne()
+    console.log('calendar removed')
+    res.status(204).send()
+});
+
+
+router.post('', async (req, res) => {
+
+	let calendario = new Calendario({
+        nome: req.params.nome
+    });
+    
+	calendario = await calendario.save();
+    
+    let idCalendario = calendario.idCalendario;
+
+    console.log('calendar saved successfully');
+
+    res.location("/api/v1/books/" + idCalendario).status(201).send();
+});
 
 
 module.exports = router;
--- backend/models/utente.js
+++ backend/models/utente.js
@@ -1,12 +1,26 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+// tutti i tipi di utenti sono unificati in un solo tipo di utente
+// se degli attributi non lo riguardano vengono impostati a null
 new Schema({
     idUtente: String,
     nome: String,
     cognome: String,
     idCalendario: String,
-    chat: [String]
+    chat: [String],
+    role: String,
+    abbonamento: {
+        descrizione: String,
+        dataInizio: Date,
+        durata: Number // durata in giorni
+    },
+    // se è un abbonato le schede e le valutazioni sono le sue personali
+    // se è un Personale di sala le scede e le valutazioni sono quelle che ha inserito
+    // inseriamo propieteario schede e valutazioni direttamente nelle schede e nelle valutazioni 
+    // schede: [String],
+    // valutazioni: [String]
+    idPalestra: String,
 })
 
 module.exports = mongoose.model('utente', Schema);
