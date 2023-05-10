177cfd473f25290a893fa1f4234628457ac4502e
Lorenzo Fasol <67193256+Lory1403@users.noreply.github.com>
2023-05-10 15:34:44 +0200

    autenticazione

--- backend/app/app.js
+++ backend/app/app.js
@@ -20,8 +20,11 @@ app.use('api/v1/utenti', tokenChecker);
 
 
 // api nostre che usiamo
+
+app.use('/authenticate/google');
+
 app.use('/api/v1/authentications', authentication);
-app.use('api/v1/utenti', utenti);
+app.use('/api/v1/utenti', utenti);
 app.use('/api/v1/palestre', palestre);
 app.use('/api/v1/abbonamento', abbonamento);
 app.use('/api/v1/calendario', calendario); // dentro a calendario usiamo la api di appuntamento
@@ -31,6 +34,30 @@ app.use('/api/v1/calendario', calendario); // dentro a calendario usiamo la api 
 app.use((req,res,next) => {
     console.log(req.method + ' ' + req.url)
     next()
+});
+
+//autenticazione con Google
+app.post('/login', (req,res,next) => {
+    passport.authenticate('local', {
+        failureRedirect: '/login',
+        successRedirect: '/api/v1/hello',
+        failureFlash: true,
+    })(req,res,next);
+});
+
+app.post('/logout', (req,res) => {
+    req.logout();
+    req.session.destroy(function(err) {
+        res.redirect('/');
+    });
+});
+
+app.get('/google', (req,res) => {
+
+});
+
+app.get('/google/callback', (req,res) => {
+
 });
 
 app.get('/api/v1/hello', (req,res) => {
--- backend/app/authentication.js
+++ backend/app/authentication.js
@@ -1,5 +1,21 @@
-const express = require('express');
-const router = express.Router;
+//const express = require('express');
+//const router = express.Router;
+//const jwt = require('jsonwebtoken');
 
+import * as queryString from 'query-string';
 
-module.exports = router;
+const stringifiedParams = queryString.stringify({
+  client_id: process.env.CLIENT_ID_GOES_HERE,
+  redirect_uri: 'https://prova-i1d6.onrender.com/authenticate/google',
+  scope: [
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile',
+  ].join(' '), // space seperated string
+  response_type: 'code',
+  access_type: 'offline',
+  prompt: 'consent',
+});
+
+const googleLoginUrl = `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`;
+
+//module.exports = router;
