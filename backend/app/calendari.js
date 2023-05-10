bdf2faedacfe579e584cc1716890c2ad8ba1b633
Riccardo Minella <67193261+RiccardoMinella@users.noreply.github.com>
2023-05-10 16:33:25 +0200

    api test

--- backend/app/calendari.js
+++ backend/app/calendari.js
@@ -3,7 +3,22 @@ const router = express.Router();
 const calendario = require('./models/calendario');
 
 
+router.get('', async (req, res) => {
+    
+    // https://mongoosejs.com/docs/api.html#model_Model.find
+    let calendari = await calendario.find({});
 
+    calendari = calendari.map( (calendario) => {
+        return {
+            self: '/api/v1/calendari/' + calendario.idCalendario,
+            nome: calendario.nome
+        };
+    });
+
+    res.status(200).json(calendari);
+});
+
+router.get('/:id',)
 
 
 
