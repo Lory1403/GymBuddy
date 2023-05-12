2a782a21bafcf6a98b6756536bd2cbfc1ee6a1d8
Riccardo Minella <67193261+RiccardoMinella@users.noreply.github.com>
2023-05-12 19:31:26 +0200

    aggiunte alcune api utenti, calendari e appuntamenti

--- backend/app/utenti.js
+++ backend/app/utenti.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const Utenti = require('./models/utente'); 
+const Utente = require('./models/utente'); 
+const Calendario = require('./models/calendario');
 
 
+
+
+
+// post per creare un utente
+
+router.post('', async (req,res) => {
+
+    let calendario = new Calendario({
+        nome: 'My calendar'
+    })
+
+    let utente = new Utente({
+        nome: req.body.nome,
+        cognome: req.body.cognome,
+        email: req.body.email,
+        idCalendario: calendario._id,
+        role: req.body.role,
+    });
+
+    calendario.save();
+    utente.save();
+
+    idUtente = utente._id;
+
+    console.log('user saved successfully');
+
+    res.location("/api/v1/utenti/" + idUtente).status(201).send();
+});
 
 
 // metodi da aggiungere:
