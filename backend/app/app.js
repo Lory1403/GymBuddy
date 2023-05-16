069c0f828ddeb2997e71b7739bcf19b676359f3b
Lorenzo Fasol <67193256+Lory1403@users.noreply.github.com>
2023-05-16 15:11:57 +0200

    Modifiche dopo il merge

--- backend/app/app.js
+++ backend/app/app.js
@@ -3,14 +3,15 @@ const express = require("express");
 const app = express();
 //const user = require('./models/utente');
 //const passport = require('passport');
-const autenticazione = require('./autenticazione');
+const autenticazioni = require('./autenticazione');
 var passport = require("./google/googleAuth");
 var User = require("./models/utente");
 var autenticazioneGoogle = require('./google/autenticazioneGoogle');
 
-const utente = require("./utenti.js");
-const tokenChecker = require('./tokenChecker.js');
-const abbonamenti = require('./abbonamenti.js');
+const utenti = require("./utenti.js");
+const tokenChecker = require('./tokenChecker');
+const abbonamenti = require('./abbonamenti');
+//const palestre = require('./palestre');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,9 +21,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/v1/autenticazioni', autenticazione);
-app.use('/api/v1/utenti', utente);
-app.use('/auth', autenticazioneGoogle);
+
 
 
 // api nostre protette
@@ -32,11 +31,12 @@ app.use('/api/v1/calendario', tokenChecker);
 app.use('/api/v1/utenti', tokenChecker);
 
 //api nostre che usiamo
-app.use('/api/v1/authentications', authentication);
+app.use('/api/v1/autenticazioni', autenticazioni);
 app.use('/api/v1/utenti', utenti);
-app.use('/api/v1/palestre', palestre);
+app.use('/auth', autenticazioneGoogle);
+//app.use('/api/v1/palestre', palestre);
 app.use('/api/v1/abbonamenti', abbonamenti);
-app.use('/api/v1/calendario', calendario); // dentro a calendario usiamo la api di appuntamento
+//app.use('/api/v1/calendario', calendario); // dentro a calendario usiamo la api di appuntamento
 
 
 app.get("/api/v1/hello", (req, res) => {
