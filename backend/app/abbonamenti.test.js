d2ee2b6278f7451f16344fb6178ddeae8ac8a0e7
MartinaPanini <107107355+MartinaPanini@users.noreply.github.com>
2023-05-29 12:07:23 +0200

    test abbonamenti

---
+++ backend/app/abbonamenti.test.js
@@ -0,0 +1,24 @@
+const request  = require('supertest');
+const app      = require('./app');
+const jwt      = require('jsonwebtoken'); 
+const mongoose = require('mongoose');
+
+const abbonamenti = require('./abbonamenti'); // Importa l'API da testare
+
+//Nessun Abbonamento
+
+//Nessun abbonamento dell'utente
+
+//Utente non trovato
+
+//idPalestra non trovato
+
+//L'utente possiede già un abbonamento
+
+//Abbonamento scaduto
+
+//Abbonamento non inserito
+
+
+  
+  
