34420cccd08ecc435eb48df0105d53aeb942eaa0
MartinaPanini <107107355+MartinaPanini@users.noreply.github.com>
2023-06-09 20:33:47 +0200

    dashboard

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,17 +9,19 @@ import { ref, onMounted } from 'vue'
   <!-- Header -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark position-absolute top-0 start-0 w-100">
     <div class="container-fluid">
-      <div class="navbar-brand" href="#">
-        <img src="@/assets/logo.png" alt="logo-palestra" width="100"
-          class="d-inline-block align-text-top">
+      <!-- DA IMPLEMENTARE -->
+      <!-- Quando clicco sul logo se non sono loggato mi rimanda a pagina apertura altrimenti mi rimanda alla home-->
+      <a class="navbar-brand" href="/" onclick="handleclick">
+        <!-- fine evento click su logo-->
+        <img src="@/assets/logo.png" alt="logo-palestra" width="150" class="d-inline-block align-text-center">
         GymBuddy
-      </div>
+      </a>
 
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
         aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+     <!--  <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
           <li class="nav-item">
             <RouterLink class="nav-link" to="/home">Home</RouterLink>
@@ -31,7 +33,7 @@ import { ref, onMounted } from 'vue'
             <RouterLink class="nav-link" to="/registration">Registration</RouterLink>
           </li>
         </ul>
-      </div>
+      </div> -->
     </div>
   </nav>
   <!-- End Header -->
