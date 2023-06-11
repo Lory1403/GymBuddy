26c9cee380687a27fc46ef5e6ef4e7a249e0fe41
Lory1403 <67193256+Lory1403@users.noreply.github.com>
2023-06-11 23:43:01 +0200

    Deploy

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory('https://gymbuddy-14b9.onrender.com'),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
