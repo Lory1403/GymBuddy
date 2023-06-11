6c9c8e9a12f4c59cb75b76be596a3b838254e364
Riccardo Minella <67193261+RiccardoMinella@users.noreply.github.com>
2023-06-11 21:24:52 +0200

    Frontend compilato

---
+++ frontend/dist/assets/loggedUser-8bd3ef49.js
@@ -0,0 +1,1 @@
+import{h as i}from"./index-03c78cd7.js";const n=i({token:void 0,email:void 0,id:void 0,self:void 0});function d(e){n.token=e.token,n.email=e.email,n.id=e.id,n.self=e.self}export{n as l,d as s};
